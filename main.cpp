5bba1a0957f50194e17bce0489aa7e648d223f4b
maheshthedev <mahesh.svmr@gmail.com>
2019-04-25 12:09:06 +0530

    Updated User Friendly

--- main.cpp
+++ main.cpp
@@ -4,15 +4,12 @@ struct Process_Data
 {
 	int Num,Pid,A_time,B_time,Priority,F_time,R_time,W_time,S_time,Res_time;
 };
-
-struct Process_Data current;
+struct Process_Data c;
 typedef struct Process_Data sd ;
-
 bool idsort(sd& x ,sd& y)
 {
 	return x.Pid < y.Pid;
 }
-
 bool arrivalsort(sd& x ,sd& y)
 {
 	if(x.A_time < y.A_time)
@@ -25,16 +22,12 @@ bool arrivalsort(sd& x ,sd& y)
 		return false;
 	if(x.Pid < y.Pid)
 		return true;
-
 	return false;
 }
-
-
 bool Numsort(sd& x ,sd& y)
 {
 	return x.Num < y.Num;
 }
-
 struct comPare
 {
 	bool operator()(sd& x ,sd& y)
@@ -50,27 +43,43 @@ struct comPare
 	}
 
 };
-
 void my_check(vector<sd> mv)
 {
-	for(unsigned int i= 0; i < mv.size() ;i++)
+	for(int i= 0; i < mv.size() ;i++)
 	{
-		cout<<" Pid :"<<mv[i].Pid<<" _time : "<<mv[i].A_time<<" B_time : "<<mv[i].B_time<<" Priority : "<<mv[i].Priority<<endl;
+		cout<<"Pid :"<<mv[i].Pid<<" _time : "<<mv[i].A_time<<" B_time : "<<mv[i].B_time<<" Priority : "<<mv[i].Priority<<endl;
 	}
-
 }
-
 int main()
 {
-	int i;
-	vector<sd> ip,ipc;
+	vector<sd> ip;
 	sd temp;
-	int pq_process=0,rq_process = 0;
-	int A,B,Pid,Priority,n,clock,tet = 0;
+	int pq_process=0,rq_process = 0,i=0,A,B,Pid,Priority,n,clock,tet = 0;
+	cout<<" Enter No.of Processes:";
 	cin>>n;
+	if(n<=0)
+    {
+        system("CLS");
+        cout<<"Invalid Input";
+        exit(0);
+    }
 	for( i= 0; i< n; i++ )
 	{
-		cin>>Pid>>A>>B>>Priority;
+	    cout<<" "<<i+1<<" Process \n";
+	    cout<<"Process Id :";
+		cin>>Pid;
+		cout<<"Arrival Time:";
+		cin>>A;
+		cout<<"Burst Time:";
+		cin>>B;
+		cout<<"Priority :";
+		cin>>Priority;
+		if(Pid<0||A<0||B<0||Priority<0)
+        {
+            system("CLS");
+            cout<<"Invalid Input";
+            exit(0);
+        }
 		temp.Num = i+1;
 		temp.A_time = A;
 		temp.B_time = B;
@@ -79,7 +88,6 @@ int main()
 		temp.Priority = Priority;
 		ip.push_back(temp);
 	}
-	//ipc = ip;
 	sort( ip.begin(), ip.end(), arrivalsort );
     tet = tet + ip[0].A_time;
     for( i= 0;i< n;i++ )
@@ -104,12 +112,10 @@ int main()
 	queue< sd > rq;
 	int cpu_state = 0;
 	int quantum = 4 ;
-	current.Pid = -2;
-	current.Priority = 100000;
-
+	c.Pid = -2;
+	c.Priority = 100000;
 	for ( clock = 0; clock< tet; clock++ )
 	{
-		/**Insert the process with same Arrival time in Priority Queue**/
 		for( int j = 0; j< n ; j++ )
 		{
 			if(clock == ip[j].A_time)
@@ -117,13 +123,11 @@ int main()
 				pq.push(ip[j]);
 			}
 		}
-
-
 		if(cpu_state == 0)
 		{
 			if(!pq.empty())
 			{
-				current = pq.top();
+				c = pq.top();
 				cpu_state = 1;
 				pq_process = 1;
 				pq.pop();
@@ -131,7 +135,7 @@ int main()
 			}
 			else if(!rq.empty())
 			{
-				current = rq.front();
+				c = rq.front();
 				cpu_state = 1;
 				rq_process = 1;
 				rq.pop();
@@ -142,60 +146,50 @@ int main()
 		{
 			if(pq_process == 1 && (!pq.empty()))
 			{
-				if(pq.top().Priority < current.Priority )
+				if(pq.top().Priority < c.Priority )
 				{
-					rq.push(current);
-					current = pq.top();
+					rq.push(c);
+					c = pq.top();
 					pq.pop();
 					quantum = 4;
 				}
 			}
 			else if(rq_process == 1 && (!pq.empty()))
 			{
-				rq.push(current);
-				current = pq.top();
+				rq.push(c);
+				c = pq.top();
 				pq.pop();
 				rq_process = 0;
 				pq_process = 1;
 				quantum = 4 ;
 			}
-
-
 		}
-
-
-		if(current.Pid != -2)
+		if(c.Pid != -2)
 		{
-			current.R_time--;
+			c.R_time--;
 			quantum--;
-			Ghant[clock] = current.Pid;
-			if(current.R_time == 0)
+			Ghant[clock] = c.Pid;
+			if(c.R_time == 0)
 			{
 				cpu_state = 0 ;
 				quantum = 4 ;
-				current.Pid = -2;
-				current.Priority = 100000;
+				c.Pid = -2;
+				c.Priority = 100000;
 				rq_process = 0;
 				pq_process = 0;
 			}
 			else if(quantum == 0 )
 			{
-				rq.push(current);
-				current.Pid = -2;
-				current.Priority = 100000;
+				rq.push(c);
+				c.Pid = -2;
+				c.Priority = 100000;
 				rq_process = 0;
 				pq_process = 0;
 				cpu_state=0;
-
 			}
-
 		}
-
 	}
-
-
 	sort( ip.begin(), ip.end(), idsort );
-
 	for(int i=0;i<n;i++)
 	{
 		for(int k=tet;k>=0;k--)
@@ -204,7 +198,6 @@ int main()
 			{
 				ip[i].F_time=k+1;
 				break;
-
 			}
 		}
 	}
@@ -220,19 +213,17 @@ int main()
 			}
 		}
 	}
-
 	sort( ip.begin(), ip.end(), Numsort );
-
 	for(int i=0;i<n;i++)
 	{
 		ip[i].Res_time=ip[i].S_time-ip[i].A_time;
 		ip[i].W_time=(ip[i].F_time-ip[i].A_time)-ip[i].B_time;
 
 	}
-
+	cout<<"Process Id | Response Time | Completion Time | Waiting Time\n";
 	for(int i=0;i<n;i++)
 	{
-		cout<<ip[i].Pid<<" "<<ip[i].Res_time<<" "<<ip[i].F_time<<" "<<ip[i].W_time<<endl;
+		cout<<ip[i].Pid<<"                 "<<ip[i].Res_time<<"               "<<ip[i].F_time<<"           "<<ip[i].W_time<<endl;
 
 	}
 	return 0;
